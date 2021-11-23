fa7890e83e5d3cdce91279b55587eb2f3c8e7962
David Sehr <sehr@google.com>
2021-11-23 22:46:31 +0530

    config: enable dex2oat64 on 64-bit targets
    Bug: 153380900
    Test: boot and install application.
    Change-Id: Iac14c028c1c5c7aa4a9173d51d63ef5834ba690a
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -135,6 +135,11 @@ endif
 
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
+# Use 64-bit dex2oat for better dexopt time.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true
+endif
 
 # SystemUI plugins
 PRODUCT_PACKAGES += \
