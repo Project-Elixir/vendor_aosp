d7fe125e2fabe8789d7ed73ffda10c85d16729c4
Nishant Kumar <www.rajsonu13@gmail.com>
2022-08-18 12:17:44 +0530

    Ven/Aosp: Use new mini gms
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -219,7 +219,7 @@ $(call inherit-product, vendor/aosp/config/bootanimation.mk)
 $(call inherit-product, vendor/aosp/config/fonts.mk)
 
 # GApps
-$(call inherit-product, vendor/gapps/config.mk)
+$(call inherit-product, vendor/gms/gms_mini.mk)
 
 # OTA
 $(call inherit-product, vendor/aosp/config/ota.mk)
