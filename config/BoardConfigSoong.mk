767b233d1ad8f6ef88d12a69b26be9c3c1bbc268
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:48:39 +0300

    config: Remove TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    This hasn't been ported after lineage-17.1
    
    Change-Id: I2aff9bb7bc17fa1444a904d47248d885268bf29b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,7 +32,6 @@ SOONG_CONFIG_customGlobalVars += \
     target_alternative_futex_waiters \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
-    ignores_ftp_pptp_conntrack_failure \
     gralloc_handle_has_reserved_size \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
@@ -66,7 +65,6 @@ SOONG_CONFIG_customGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NE
 SOONG_CONFIG_customGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_customGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_customGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
-SOONG_CONFIG_customGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_customGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_customGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
