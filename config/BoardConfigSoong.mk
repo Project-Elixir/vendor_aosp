8ecbb710884ec96dd26c8861ce8e31f9ffa328a4
cjh1249131356 <cjh1249131356@gmail.com>
2023-01-15 17:52:32 +0800

    soong: Add TARGET_USES_NOTHING_CAMERA
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,7 +49,8 @@ SOONG_CONFIG_customGlobalVars += \
     uses_legacy_fd_fbdev \
     uses_egl_display_array \
     needs_oplus_tag \
-    uses_oplus_camera  
+    uses_oplus_camera \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
 SOONG_CONFIG_customNvidiaVars += \
@@ -78,6 +79,7 @@ SOONG_CONFIG_customGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGE
 SOONG_CONFIG_customGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_customGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_customGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_customGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
