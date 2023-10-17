e2adc18b2d14d045e0928fe6fe22f7449f41f166
jhenrique09 <jhsv09@gmail.com>
2023-10-17 15:25:47 -0300

    vendor: Import clocks package
    Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -232,4 +232,7 @@ $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)
 
+# Clocks
+$(call inherit-product, vendor/aosp/config/clocks.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
