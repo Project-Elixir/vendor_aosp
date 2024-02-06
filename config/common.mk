Evolution-X-AOSP/vendor_evolution

prebuilt: Add missing libpowerstatshaldataprovider and symlink for TurboAdapter_NoBatt
Ref:
https://gitlab.com/hentaiOS/platform_vendor_gms/-/commit/5765352fadfeed3d1efa97326a3cfd019d96b7b8
https://gitlab.com/hentaiOS/platform_vendor_gms/-/commit/fa86517b86e1d3b784f46c2dc4c5794a40d805d2
https://gitlab.com/hentaiOS/platform_vendor_gms/-/blob/TwistedScarlett/system_ext/packages/privileged_apps/TurboAdapter/Android.mk

config/common.mk
@@ -214,6 +214,12 @@ TARGET_USES_PICO_GAPPS ?= false
 ifeq ($(TARGET_SUPPORTS_GOOGLE_BATTERY), false)
 PRODUCT_PACKAGES += \
     TurboAdapter_NoBatt
+
+PRODUCT_COPY_FILES += \
+    vendor/evolution/prebuilt/system_ext/lib64/libpowerstatshaldataprovider.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libpowerstatshaldataprovider.so
+
+PRODUCT_PACKAGES += \
+    LibPowerStatsSymLink_NoBatt
 endif
 
 # Face Unlock

Project-Elixir/vendor_aosp

config/common.mk
@@ -124,6 +124,12 @@ TARGET_SUPPORTS_GOOGLE_BATTERY ?= false
 ifeq ($(TARGET_SUPPORTS_GOOGLE_BATTERY), false)
 PRODUCT_PACKAGES += \
     TurboAdapter_NoBatt
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/system_ext/lib64/libpowerstatshaldataprovider.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libpowerstatshaldataprovider.so
+
+PRODUCT_PACKAGES += \
+    LibPowerStatsSymLink_NoBatt
 endif
 
 # Dex preopt