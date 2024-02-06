Evolution-X-AOSP/vendor_evolution

vendor: Import clocks package from Android 14 QPR1
Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf

Co-authored-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -279,6 +279,9 @@ else ifneq ($(TARGET_USES_LEGACY_BOOTANIMATION), true)
 $(call inherit-product, vendor/evolution/config/bootanimation.mk)
 endif
 
+# Clocks
+$(call inherit-product, vendor/evolution/config/clocks.mk)
+
 # Inherit from fonts config
 $(call inherit-product, vendor/evolution/config/fonts.mk)
 

Project-Elixir/vendor_aosp

Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
Co-Authored-By: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -200,6 +200,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
+# Clocks
+$(call inherit-product, vendor/aosp/config/clocks.mk)
+
 # Pixel customization
 TARGET_SUPPORTS_GOOGLE_RECORDER ?= true
 TARGET_INCLUDE_STOCK_ARCORE ?= true